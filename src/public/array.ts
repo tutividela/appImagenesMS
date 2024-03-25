c149fec3143043b4032770439e174e906ff5e659
tutividela <exequiel.videlapm@gmail.com>
2024-03-25 20:24:42 -0300

    agrego alert en logout

---
+++ src/public/array.ts
@@ -0,0 +1,124 @@
+import { Encuesta } from "../types/types";
+
+export const encuestas: Encuesta[] = [
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "La Matanza",
+                provincia: "Buenos Aires",
+                barrio: "El Fortín",
+            },
+        },
+        _id: "61c2b3939f7fb90016e59482",
+        estado: "Rechazado",
+        apellido: "Ortiz",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                barrio: "Ing. Allan",
+                provincia: "Buenos Aires",
+                partido: "Florencio Varela",
+            },
+        },
+        _id: "618de9ce3939cd0016d41833",
+        estado: "Encuesta 3",
+        apellido: "Ape1/Ape2",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "La Matanza",
+                provincia: "Buenos Aires",
+                barrio: "El Fortín",
+            },
+        },
+        _id: "61bce6ad64bab00016959b8b",
+        estado: "Encuesta 3",
+        apellido: "P",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                barrio: "Los Hornos",
+                provincia: "Buenos Aires",
+                partido: "Moreno",
+            },
+        },
+        _id: "62329690eb579a0016588cfc",
+        estado: "Construido",
+        apellido: "Gomez",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "Pilar",
+                provincia: "Buenos Aires",
+                barrio: "Luchetti",
+            },
+        },
+        _id: "618df8723939cd0016d418ed",
+        estado: "Encuesta 3",
+        apellido: "Perez",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "La Matanza",
+                provincia: "Buenos Aires",
+                barrio: "El Fortín",
+            },
+        },
+        _id: "61c2b3939f7fb90016e59482",
+        estado: "Rechazado",
+        apellido: "Ortiz",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                barrio: "Ing. Allan",
+                provincia: "Buenos Aires",
+                partido: "Florencio Varela",
+            },
+        },
+        _id: "618de9ce3939cd0016d41833",
+        estado: "Encuesta 3",
+        apellido: "Ape1/Ape2",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "La Matanza",
+                provincia: "Buenos Aires",
+                barrio: "El Fortín",
+            },
+        },
+        _id: "61bce6ad64bab00016959b8b",
+        estado: "Encuesta 3",
+        apellido: "P",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                barrio: "Los Hornos",
+                provincia: "Buenos Aires",
+                partido: "Moreno",
+            },
+        },
+        _id: "62329690eb579a0016588cfc",
+        estado: "Construido",
+        apellido: "Gomez",
+    },
+    {
+        encuestaUno: {
+            direccion: {
+                partido: "Pilar",
+                provincia: "Buenos Aires",
+                barrio: "Luchetti",
+            },
+        },
+        _id: "618df8723939cd0016d418ed",
+        estado: "Encuesta 3",
+        apellido: "Perez",
+    },
+];
